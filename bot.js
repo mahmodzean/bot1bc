0e5eea7820f303d6ded283487deed03fa41c3a49
mahmodzean <44239884+mahmodzean@users.noreply.github.com>
2018-11-07 18:46:34 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc1') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+client.login('NDkyNDEzMTYyNDg3MDIxNTY4.DsSn4A.x53aveaE_XZvJI_GpqBrjFAF8RE');
